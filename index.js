c762f5dacca3f48196ca5fa12d4357c0cae47b15
Yoo2309 <dutran2392002@gmail.com>
2024-06-23 01:18:02 +0700

    feat-vercel

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 app.get("/proxy", (req, res) => {
   const url = req.query.url;
   if (!url) {
     return res.status(400).send("URL is required");
   }
+  ``;
 
   request(url).pipe(res);
 });
