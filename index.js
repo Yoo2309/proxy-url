8b98470c017897f72a731631e38a7fe6cd98abce
Yoo2309 <dutran2392002@gmail.com>
2024-06-22 23:55:43 +0700

    feat-proxy

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get("/proxy", (req, res) => {
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send("URL is required");
+  }
+
+  request(url).pipe(res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
+});
